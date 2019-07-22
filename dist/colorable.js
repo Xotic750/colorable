23d9d6a81eb91beb3d7ebdd20d0dcc54ee8fcbd9
Graham Fairweather <xotic750@gmail.com>
2019-07-22 19:47:04 +0200

    :bookmark: v1.0.19
    Auto build

--- dist/colorable.js
+++ dist/colorable.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) Graham Fairweather",
-  "date": "2019-07-22T15:26:07.417Z",
+  "date": "2019-07-22T17:46:57.091Z",
   "describe": "",
   "description": "Color palette combination contrast tester",
   "file": "colorable.js",
   "hash": "59871b4c493a55790e79",
   "license": "MIT",
-  "version": "1.0.18"
+  "version": "1.0.19"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
